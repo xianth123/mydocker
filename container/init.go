21ad33f2a6937eac6f9d33b5d21f4121a1e03bd5
李志席 <xianth123@qq.com>
2019-09-08 19:27:10 +0800

    add pivot_root change the root mount

--- container/init.go
+++ container/init.go
@@ -3,6 +3,7 @@ package container
 import (
 	"fmt"
 	"os/exec"
+	"path/filepath"
 
 	//	"github.com/sirupsen/logrus"
 	"os"
@@ -23,9 +24,8 @@ func RunContainerInitProcess() error {
 	if cmdArray == nil || len(cmdArray) == 0 {
 		return fmt.Errorf("Run container get user command error, cmdArray is nil")
 	}
-	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
-	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
-	// 寻找命令的路径
+
+	setUpMount()
 	path, err := exec.LookPath(cmdArray[0])
 	if err != nil {
 		log.Errorf("exec loop path error %v", err)
@@ -48,4 +48,54 @@ func readUserCommand() []string  {
 	}
 	msgStr := string(msg)
 	return strings.Split(msgStr, " ")
+}
+
+// Init 挂载点
+func setUpMount()  {
+	pwd ,err := os.Getwd()
+	if err != nil {
+		fmt.Println("Get current location error %v", err)
+		return
+	}
+	fmt.Println("current location is %s", pwd)
+	pivotRoot(pwd)
+
+	// mount proc
+	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+	syscall.Mount("tmpfs", "/dev", "tmpfs", syscall.MS_NOSUID|syscall.MS_STRICTATIME, "mode=755")
+}
+
+
+func pivotRoot(root string) error {
+	// 将 root 重新 mount 一次， 然后，将 root 使用 bind 换一个挂载的地点。然后可以解除 root 的挂载
+	// func Mount(source string, target string, fstype string, flags uintptr, data string) (err error)
+	// #define MS_BIND      4096
+	///* 把一个已挂载文件系统移动到另一个挂载点，相当于先执行卸载，然后将文件系统挂载在另外的一个目录下 */
+	// #define MS_REC       16384 /* 为目录子树递归的创建绑定挂载 */
+	if err := syscall.Mount(root, root, "bind", syscall.MS_BIND|syscall.MS_REC); err != nil {
+		return fmt.Errorf("Mount rootfs to itself error: %v", err)
+	}
+	// 创建 rootfs/.pivot_root 存储 old_root
+	pivotDir := filepath.Join(root, ".pivot_root")
+	if err := os.Mkdir(pivotDir, 0777); err != nil {
+		return nil
+	}
+	// pivot_root 到显得 rootfs ，old_root 是挂载到 rootfs/.pivot_root
+	// 挂载点现在可以在 mount 命令中看到
+	if err := syscall.PivotRoot(root, pivotDir); err != nil {
+		return fmt.Errorf("pivot_root %v", err)
+	}
+	if err := syscall.Chdir("/"); err != nil {
+		return fmt.Errorf("chdir / %v", err)
+	}
+
+	pivotDir = filepath.Join("/", ".pivot_root")
+	// umount rootfs/.pivot_root
+	if err := syscall.Umount(pivotDir, syscall.MNT_DETACH); err != nil {
+		return fmt.Errorf("umount pivot_root fail %v", err)
+	}
+	return os.Remove(pivotDir)
+
+
 }
